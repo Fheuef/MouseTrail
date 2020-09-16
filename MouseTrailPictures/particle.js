c0fe8cdc8db0eb53007cba193af88d6acd5456b3
Godeta <Godeta61@gmail.com>
2020-09-16 11:46:43 +0200

    Remise de l'ancienne version avant de rajouter la dernière avec les planètes

--- MouseTrailPictures/particle.js
+++ MouseTrailPictures/particle.js
@@ -1,7 +1,7 @@
 var defaultSpeed = 10;
 
 class Particle {
-	constructor(x = 0, y = 0, speed = defaultSpeed) {
+	constructor(x=0, y=0, speed = defaultSpeed) {
 		this.pos = new Vector2(x, y);
 		this.vel = new Vector2(0, 0);
 		this.speed = speed;
@@ -18,18 +18,4 @@ class Particle {
 	updatePos() {
 		this.pos = this.pos.add(this.vel);
 	}
-	//si il touche un objet de type particule eater (ou particule mais avec un attribut taille)
-	hit(eater) {
-		/*si il est entre l'abscisse de l'objet et son abscisse + sa taille (largeur et hauteur pareils car ellipse)
-		et entre son ordonné et son ordonné + sa taille, donc si il rentre dans le cercle de l'objet
-		*/
-		if (this.pos.x > eater.pos.x && this.pos.x < eater.pos.x + eater.size &&
-			this.pos.y > eater.pos.y && this.pos.y < eater.pos.y + eater.size) {
-			return true;
-		}
-		//console.log("no hit");
-		return false;
-
-	}
-
 }
