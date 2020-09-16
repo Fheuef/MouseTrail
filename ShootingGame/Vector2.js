0065522a178354013128f7d185b9e1b330ad160e
Godeta <Godeta61@gmail.com>
2020-09-16 19:30:15 +0200

    Reduction taille images + début jeu shootingGame

---
+++ ShootingGame/Vector2.js
@@ -0,0 +1,56 @@
+class Vector2 {
+	constructor(x=0, y=0) {
+		this.x = x;
+		this.y = y;
+	}
+
+	set(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	length() {
+		return Math.sqrt(this.x * this.x + this.y * this.y);
+	}
+
+	add(vec) {
+		return new Vector2(this.x + vec.x, this.y + vec.y);
+	}
+
+	subtract(vec) {
+		return new Vector2(this.x - vec.x, this.y - vec.y);
+	}
+
+	negative() {
+		return new Vector2(-this.x, -this.y);
+	}
+
+	dot(vec) {
+		return this.x * vec.x + this.y * vec.y;
+	}
+
+	multiply(n) {
+		return new Vector2(this.x * n, this.y * n);
+	}
+
+	divide(n) {
+		return new Vector2(this.x / n, this.y / n);
+	}
+
+	normalized() {
+		let n = this.length();
+		return this.divide(n);
+	}
+
+	clampLength(n) {
+		let len = this.length();
+		if (len > n)
+			return this.divide(len).multiply(n);
+		else
+			return this.clone();
+	}
+
+	clone() {
+		return new Vector2(this.x, this.y);
+	}
+}
